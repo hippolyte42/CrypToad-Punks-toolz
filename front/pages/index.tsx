8d6f9834b53665705c8859dd701fe279e1f638e0
buyhighHippo <hippolytejacque@hotmail.fr>
2022-02-19 15:53:03 +0100

    fix twitter:image url

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
 
         <meta property="twitter:title" content="crypToad Punks" />
         <meta property='twitter:description' content='6969 CrypToad Punks on the Ethereum blockchain. No roadmap. Just !vibes. CC0 public domain project.'/>
-        <meta property="twitter:image" content="https://imgur.com/CUx4oyz" />
+        <meta property="twitter:image" content="./toad-punks-2.png" />
         <meta property="twitter:image:alt" content="6969 CrypToad Punks on the Ethereum blockchain. No roadmap. Just !vibes. CC0 public domain project." />
 
         <meta property='twitter:site' content='@cryptoadpunks'/>
