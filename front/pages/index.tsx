f63662a425fa88eeb3253f54e4ed74f5fe5ad41a
buyhighHippo <hippolytejacque@hotmail.fr>
2022-02-20 01:33:41 +0100

    styling: spacing and font size adjustments

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -22,54 +22,54 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <img className="w-1/2 min-w-[325px]" src="./toad-punks.png"/>
+        <img className="w-1/2 min-w-[325px] mb-9" src="./toad-punks.png"/>
         
         <div className='items-start w-1/2 min-w-[325px] text-left'>
           <br/><br/>
-          <h1 className="text-3xl font-bold text-larva-pink">
+          <h1 className="text-6xl font-bold text-larva-pink mb-9">
             CrypToad Punks
           </h1>
           <br/>
-          <div className="mt-6 flex max-w-4xl flex-wrap sm:w-full mb-6 text-base">
+          <div className="mt-6 flex max-w-4xl flex-wrap sm:w-full mb-6 text-lg">
             <div className='items-start mb-12 w-full'>
               <a
                 href="https://opensea.io/collection/cryptoadpunks"
-                className="underline mb-6 text-base"
+                className="underline mb-6"
                 target={"_blank"}
               >OpenSea</a>{' '}∙{' '}
               <a
                 href="https://looksrare.org/collections/0x4f9835DBB442b263BBd20FF2731162101d9C3A89"
-                className="underline mb-6 text-base"
+                className="underline mb-6"
                 target={"_blank"}
               >LooksRare</a>{' '}∙{' '}
               <a
                 href="https://etherscan.io/address/0x4f9835DBB442b263BBd20FF2731162101d9C3A89"
-                className="underline mb-6 text-base"
+                className="underline mb-6"
                 target={"_blank"}
               >Etherscan</a>{' '}∙{' '}
               <a
                 href="https://twitter.com/cryptoadpunks"
-                className="underline mb-6 text-base"
+                className="underline mb-6"
                 target={"_blank"}
               >Twitter</a>{' '}∙{' '}
               <a
                 href="https://discord.gg/cryptoadpunks"
-                className="underline text-base"
+                className="underline"
                 target={"_blank"}
               >Discord</a>{' '}∙{' '}
               <a
                 href="https://github.com/buyhighHippo/CrypToad-Punks-toolz"
-                className="underline text-base"
+                className="underline"
                 target={"_blank"}
               >GitHub</a>
             </div>
             {/* OpenSea ∙ LooksRare ∙ Etherscan ∙ Twitter ∙ Discord */}
-            <p className="mb-6 text-base">
+            <p className="mb-6">
               6969 CrypToad Punks on the Ethereum blockchain. No roadmap. Just !vibes. CC0 public domain project.
             </p>
-            <p className="text-base ">80% of mint proceeds (total of 69eth) was donated to the <a
+            <p>80% of mint proceeds (total of 69eth) was donated to the <a
                 href="https://www.cryptoadz.io/"
-                className="underline mb-6 text-base"
+                className="underline mb-6"
                 target={"_blank"}
               >CrypToadz</a> treasury.</p>
           </div>
