e9eb944379b9558b6a41da09c0653192576d3815
buyhighHippo <hippolytejacque@hotmail.fr>
2022-02-24 23:50:55 +0100

    draft

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -9,29 +9,28 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
 
         <meta property='twitter:card' content='summary_large_image'/>
-
         <meta property="twitter:title" content="CrypToad Punks" />
         <meta property='twitter:description' content='6969 CrypToad Punks on the Ethereum blockchain. No roadmap. Just !vibes. CC0 public domain project.'/>
         <meta property="twitter:image" content="https://i.imgur.com/y3VvS8q.jpeg" />
         <meta property="twitter:image:alt" content="6969 CrypToad Punks on the Ethereum blockchain. No roadmap. Just !vibes. CC0 public domain project." />
-
         <meta property='twitter:site' content='@cryptoadpunks'/>
         <meta property='twitter:site:id' content='1491380727006433282'/>
         <meta property='twitter:creator' content='@buyhighHippo'/>
         <meta property='twitter:creator:id' content='1431037191279493123'/>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <img className="w-1/2 min-w-[325px] mb-9" src="./toad-punks.png"/>
-        
-        <div className='items-start w-1/2 min-w-[325px] text-left'>
-          <br/><br/>
-          <h1 className="text-4xl md:text-6xl font-bold text-larva-pink mb-9">
+      <main className="flex w-full flex-1 flex-col items-center px-20 mt-40 mb-24">
+
+        <img className="w-1/2 min-w-[325px] mb-14 md:mb-20" src="./toad-punks.png"/>
+
+        <div className='items-start w-1/2 min-w-[325px] text-left mb-32'>
+          <h1 className="text-4xl md:text-6xl font-bold text-larva-pink mb-14 md:mb-20">
             CrypToad Punks
           </h1>
-          <br/>
-          <div className="mt-6 flex max-w-4xl flex-wrap sm:w-full mb-6 text-lg">
+          
+          <div className="mt-6 flex max-w-4xl flex-wrap sm:w-full text-lg">
             <div className='items-start mb-12 w-full'>
+
               <a
                 href="https://opensea.io/collection/cryptoadpunks"
                 className="underline mb-6"
@@ -63,17 +62,44 @@ const Home: NextPage = () => {
                 target={"_blank"}
               >GitHub</a>
             </div>
-            {/* OpenSea ∙ LooksRare ∙ Etherscan ∙ Twitter ∙ Discord */}
+            
             <p className="mb-6">
               6969 CrypToad Punks on the Ethereum blockchain. No roadmap. Just !vibes. CC0 public domain project.
             </p>
-            <p>80% of mint proceeds (total of 69eth) was donated to the <a
+            
+            <p className="">80% of mint proceeds (total of 69eth) was donated to the <a
                 href="https://www.cryptoadz.io/"
                 className="underline mb-6"
                 target={"_blank"}
               >CrypToadz</a> treasury.</p>
           </div>
         </div>
+
+        <div className='justify-evenly md:flex'>
+              <div className='p-12'>
+                <a  href='https://dune.xyz/rantum/NFT-Collection-Dashboard?contract_address=x4f9835dbb442b263bbd20ff2731162101d9c3a89' target={"_blank"}>
+                  <img className='mb-6 border-black border-solid border-4 rounded' src='/duneDashboard.png'/>
+                </a>
+                <h6 className='text-xl font-bold text-larva-pink mb-1'>tool title</h6>
+                <span className=''>coucou je suis un lorem pour un outils</span>
+              </div>
+              
+              <div className='p-12' >
+                <a href='https://dune.xyz/rantum/NFT-Collection-Dashboard?contract_address=x4f9835dbb442b263bbd20ff2731162101d9c3a89' target={"_blank"}>
+                  <img className='mb-6 border-black border-solid border-4 rounded' src='/duneDashboard.png'/>
+                </a>
+                <h6 className='text-xl font-bold text-larva-pink mb-1'>tool title</h6>
+                <span className=''>coucou je suis un lorem pour un outils</span>
+              </div>
+              
+              <div className='p-12'>
+                <a  href='https://dune.xyz/rantum/NFT-Collection-Dashboard?contract_address=x4f9835dbb442b263bbd20ff2731162101d9c3a89' target={"_blank"}>
+                  <img className='mb-6 border-black border-solid border-4 rounded' src='/duneDashboard.png'/>
+                </a>
+                <h6 className='text-xl font-bold text-larva-pink mb-1'>tool title</h6>
+                <span className=''>coucou je suis un lorem pour un outils</span>
+              </div>
+            </div>
       </main>
     </div>
   )
