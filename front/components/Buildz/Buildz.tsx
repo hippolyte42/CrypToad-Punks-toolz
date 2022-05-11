00561b8f5aae27bef336d19cb6129da35364e421
buyhighHippo <hippolytejacque@hotmail.fr>
2022-05-11 19:48:49 +0200

    dimention fix

--- front/components/Buildz/Buildz.tsx
+++ front/components/Buildz/Buildz.tsx
@@ -11,17 +11,17 @@ const Buildz = (): JSX.Element => {
 
   return (
     <>
-      <div className="md:flex w-screen md:w-4/6 h-full overflow-scroll overflow-y-auto overflow-x-auto p-16 bg-gray-200 rounded-2xl mb-12">
+      <div className="md:flex w-screen lg:w-5/6 h-full overflow-scroll overflow-y-auto overflow-x-auto p-16 bg-gray-200 rounded-2xl mb-12">
         
         <div className="w-full flex flex-col md:grid grid-cols-[1fr,1fr] md:grid-cols-6 max-w-screen-lg gap-8 items-center justify-center text-center">
 
-          <h1 className="text-xl md:text-3xl mb-9 w-[16em] font-bold text-larva-pink">
+          <h1 className="text-2xl md:text-3xl mb-9 w-[16em] font-bold text-larva-pink">
               MAKE YOUR CRYPTOAD PUNK
           </h1>
           <script type="text/javascript" src="/main.js" defer></script>
           
           <div className="canvasWrap md:col-start-1 md:col-end-4">
-            <canvas id="canvas" className="aspect-square rounded-2xl w-full max-w-md mx-auto bg-white border shadow-xl"></canvas>
+            <canvas id="canvas" className="aspect-square rounded-2xl w-full min-w-[22em] max-w-md mx-auto bg-white border shadow-xl"></canvas>
           </div>
           
           <div className="selectionWrap mx-auto md:col-start-4 md:col-end-7"></div>
